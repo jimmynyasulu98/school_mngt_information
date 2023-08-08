7f01ef51ad6b07b455ae5c1f2dd026e2e8701c5d
jimmynyasulu98 <jimmynyasulu98@gmail.com>
2023-08-08 19:45:00 +0200

    student assessments

--- app/controllers/assessments/staff_assessments_controller.rb
+++ app/controllers/assessments/staff_assessments_controller.rb
@@ -1,4 +1,19 @@
 class Assessments::StaffAssessmentsController < ApplicationController
+
   def new
+    @term = Term.last
+    if !StaffSubject.where("staff_id = ? AND subject_id = ? AND form_id = ? AND term_id = ?",
+      current_staff.id,params[:subject_id], params[:form], @term.id).take.nil?
+
+       @student_subjects = StudentSubject.where("subject_id = ? AND form_id = ? AND term_id = ?",
+        params[:subject_id], params[:form], @term.id)
+
+    else
+      redirect_back_or_to root_path,  alert: 'You do not have permissions'
+    end
   end
+
+  def create
+  end
+
 end
